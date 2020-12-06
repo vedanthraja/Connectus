ae2b91f3e999f0abd67fee14597ecc6ae91fd62b
vedanthraja <vedanth.raja@gmail.com>
2020-12-06 20:19:28 +0530

    Added login

---
+++ connec/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout")
+]
